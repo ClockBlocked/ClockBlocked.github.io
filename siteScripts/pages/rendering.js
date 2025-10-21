98ebd2f4c02e2ee2874fecf9b35994bc871b0b8e
Cole Hanson <4theloveofcarl@gmail.com>
2025-10-21 06:49:01 -0500

    Update rendering.js

--- siteScripts/pages/rendering.js
+++ siteScripts/pages/rendering.js
@@ -362,78 +362,73 @@ isValidRoute: (routeName, params = {}) => {
   }
 },
 
-  pages: {
-    loadHomePage: () => {
-      pageLoader.start({ message: "Loading Music..." });
-      
-      if (appState.homePageManager) {
-        const dynamicContent = $byId(IDS.dynamicContent);
-        if (dynamicContent) {
-          dynamicContent.innerHTML = "";
+pages: {
+  loadHomePage: () => {
+    pageLoader.start({ message: "Loading Music..." });
+    
+    viewManager.switchView('home').then(() => {
+      pageUpdates.breadCrumbs(
+        [
+          {
+            text: "Home",
+            route: ROUTES.HOME,
+            active: true,
+            isHome: true,
+            icon: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512"><path d="M125.2 16.1c6.2-4.4 5.4-14.8-2.2-15.6c-3.6-.4-7.3-.5-11-.5C50.1 0 0 50.1 0 112s50.1 112 112 112c32.1 0 61.2-13.7 81.5-35.5l-1.2-1.7zm0 0"/></svg>'
+          }
+        ],
+        {
+          showIcons: true,
+          truncateAfter: 3,
+          animateChanges: true,
+          schemaMarkup: true
         }
-
-        setTimeout(() => {
-          appState.homePageManager.renderHomePage();
-          
-          pageUpdates.breadCrumbs(
-            [
-              {
-                text: "Home",
-                route: ROUTES.HOME,
-                active: true,
-                isHome: true,
-                icon: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512"><path d="M125.2 16.1c6.2-4.4 5.4-14.8-2.2-15.6c-3.6-.4-7.3-.5-11-.5C50.1 0 0 50.1 0 112s50.1 112 112 112c32.1 0 61.1-13.5 81.5-35.2c5.2-5.6-1-14-8.6-13.2c-2.9 .3-5.9 .4-9 .4c-48.6 0-88-39.4-88-88c0-29.7 14.7-55.9 37.2-71.9zm289.9 85.3c-8.8-7.2-21.5-7.2-30.3 0l-216 176c-10.3 8.4-11.8 23.5-3.4 33.8s23.5 11.8 33.8 3.4L224 294.4 224 456c0 30.9 25.1 56 56 56l240 0c30.9 0 56-25.1 56-56l0-161.6 24.8 20.2c10.3 8.4 25.4 6.8 33.8-3.4s6.8-25.4-3.4-33.8l-216-176zM528 255.3L528 456c0 4.4-3.6 8-8 8l-240 0c-4.4 0-8-3.6-8-8l0-200.7L400 151 528 255.3zM352 312l0 48c0 13.3 10.7 24 24 24l48 0c13.3 0 24-10.7 24-24l0-48c0-13.3-10.7-24-24-24l-48 0c-13.3 0-24 10.7-24 24zM248.5 12.3L236.6 44.6 204.3 56.5c-7 2.6-7 12.4 0 15l32.3 11.9 11.9 32.3c2.6 7 12.4 7 15 0l11.9-32.3 32.3-11.9c7-2.6 7-12.4 0-15L275.4 44.6 263.5 12.3c-2.6-7-12.4-7-15 0zm-145 320c-2.6-7-12.4-7-15 0L76.6 364.6 44.3 376.5c-7 2.6-7 12.4 0 15l32.3 11.9 11.9 32.3c2.6 7 12.4 7 15 0l11.9-32.3 32.3-11.9c7-2.6 7-12.4 0-15l-32.3-11.9-11.9-32.3z"/></svg>'
-              }
-            ],
-            {
-              showIcons: true,
-              truncateAfter: 3,
-              animateChanges: true,
-              schemaMarkup: true
-            }
-          );
-          
-          setTimeout(() => {
-            pageLoader.complete();
-          }, 700);
-          
-          utils.scrollToTop();
-        }, 200);
-      }
-    },
-
-    loadArtistPage: (artistData, targetAlbumName = null) => {
-      pageLoader.start({ message: "Finding Artist..." });
+      );
       
-      const dynamicContent = $byId(IDS.dynamicContent);
-      if (!dynamicContent) return;
-
-      dynamicContent.innerHTML = "";
-
-      setTimeout(() => {
-        navigation.rendering.renderArtistPage(artistData, targetAlbumName);
-        pageLoader.complete();
-        utils.scrollToTop();
-      }, 200);
-    },
-
-    loadAllArtistsPage: () => {
-      pageLoader.start({ message: "Loading library..." });
-      
-      const dynamicContent = $byId(IDS.dynamicContent);
-      if (!dynamicContent || !window.music) return;
-
-      dynamicContent.innerHTML = "";
-
-      setTimeout(() => {
-        navigation.rendering.renderAllArtistsPage();
-        
-        setTimeout(() => {
-          pageLoader.complete();
-        }, 100);
-      }, 300);
-    }
+      pageLoader.complete();
+      utils.scrollToTop();
+    });
   },
+
+  loadArtistPage: (artistData, targetAlbumName = null) => {
+    pageLoader.start({ message: "Finding Artist..." });
+    
+    viewManager.switchView('artist', { artistData }).then(() => {
+      pageUpdates.breadCrumbs(
+        [
+          { 
+            text: "Home  ", 
+            route: ROUTES.HOME, 
+            active: false, 
+            isHome: true, 
+            icon: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path class="fa-secondary" d="M80 202.9L80 448c0 26.5 21.5 48 48 48l80 0 0-168c0-13.3 10.7-24 24-24l112 0c13.3 0 24 10.7 24 24l0 168 80 0c26.5 0 48-21.5 48-48l0-245.1-176-141L80 202.9z"/></svg>'
+          },
+          { 
+            text: "  Discography", 
+            route: ROUTES.ARTIST, 
+            artist: artistData.artist, 
+            active: true, 
+            icon: '' 
+          }
+        ],
+        { showIcons: true, truncateAfter: 3, animateChanges: true, schemaMarkup: true }
+      );
+      
+      pageLoader.complete();
+      utils.scrollToTop();
+    });
+  },
+
+  loadAllArtistsPage: () => {
+    pageLoader.start({ message: "Loading library..." });
+    
+    viewManager.switchView('allArtists').then(() => {
+      navigation.rendering.renderAllArtistsPage();
+      
+      pageLoader.complete();
+    });
+  }
+},
 
   rendering: {
     renderArtistPage: (artistData, targetAlbumName = null) => {
