f3e3441d13007022a5c61d6f9e0b6685cab1eb69
Cole Hanson <4theloveofcarl@gmail.com>
2025-10-23 19:13:38 -0500

    update siteScripts/utilities/templates.js

--- siteScripts/utilities/templates.js
+++ siteScripts/utilities/templates.js
@@ -489,114 +489,61 @@ case "enhancedArtist":
           <div id="artists-grid" class="artists-grid grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6 px-4 sm:px-6"></div>
         `;
   
-  case "home_bento":
-
-        return `
-
-        <div class="bento-grid">
-
-            <div class="bento-card" data-loader="true">
-
-              <div class="card-header">
-
-                <h2 class="card-title">Recently Played</h2>
-
-                <a href="#" class="card-link" data-view="recent">View All</a>
-
-              </div>
-
-              <div id="${data.IDS.recentlyPlayedSection}" class="card-content">
-
-                <div class="skeleton-loader"></div>
-
-              </div>
-
-            </div>
-
-            
-
-            <div class="bento-card bento-span-2" data-loader="true">
-
-              <div class="card-header">
-
-                <h2 class="card-title">Discover Albums</h2>
-
-                <a href="#" class="card-link" data-view="albums">Explore More</a>
-
-              </div>
-
-              <div id="${data.IDS.randomAlbumsSection}" class="card-content">
-
-                <div class="skeleton-loader"></div>
-
-              </div>
-
-            </div>
-
-            
-
-            <div class="bento-card" data-loader="true">
-
-              <div class="card-header">
-
-                <h2 class="card-title">Favorite Artists</h2>
-
-                <a href="#" class="card-link" data-view="favorite-artists">View All</a>
-
-              </div>
-
-              <div id="${data.IDS.favoriteArtistsSection}" class="card-content">
-
-                <div class="skeleton-loader"></div>
-
-              </div>
-
-            </div>
-
-            
-
-            <div class="bento-card" data-loader="true">
-
-              <div class="card-header">
-
-                <h2 class="card-title">Your Playlists</h2>
-
-                <a href="#" class="card-link" data-view="playlists">View All</a>
-
-              </div>
-
-              <div id="${data.IDS.playlistsSection}" class="card-content">
-
-                <div class="skeleton-loader"></div>
-
-              </div>
-
-            </div>
-
-            
-
-            <div class="bento-card" data-loader="true">
-
-              <div class="card-header">
-
-                <h2 class="card-title">Favorite Songs</h2>
-
-                <a href="#" class="card-link" data-view="favorite-songs">View All</a>
-
-              </div>
-
-              <div id="${data.IDS.favoriteSongsSection}" class="card-content">
-
-                <div class="skeleton-loader"></div>
-
-              </div>
-
-            </div>
-
-          </div>
-
-        `;
-
+// In the home_bento case, update the IDs to match what your homeSection functions expect
+case "home_bento":
+  return `
+  <div class="bento-grid">
+      <div class="bento-card" data-loader="true">
+        <div class="card-header">
+          <h2 class="card-title">Recently Played</h2>
+          <a href="#" class="card-link" data-view="recent">View All</a>
+        </div>
+        <div id="recentlyPlayedSection" class="card-content">
+          <div class="skeleton-loader"></div>
+        </div>
+      </div>
+      
+      <div class="bento-card bento-span-2" data-loader="true">
+        <div class="card-header">
+          <h2 class="card-title">Discover Albums</h2>
+          <a href="#" class="card-link" data-view="albums">Explore More</a>
+        </div>
+        <div id="randomAlbumsSection" class="card-content">
+          <div class="skeleton-loader"></div>
+        </div>
+      </div>
+      
+      <div class="bento-card" data-loader="true">
+        <div class="card-header">
+          <h2 class="card-title">Favorite Artists</h2>
+          <a href="#" class="card-link" data-view="favorite-artists">View All</a>
+        </div>
+        <div id="favoriteArtistsSection" class="card-content">
+          <div class="skeleton-loader"></div>
+        </div>
+      </div>
+      
+      <div class="bento-card" data-loader="true">
+        <div class="card-header">
+          <h2 class="card-title">Your Playlists</h2>
+          <a href="#" class="card-link" data-view="playlists">View All</a>
+        </div>
+        <div id="playlistsSection" class="card-content">
+          <div class="skeleton-loader"></div>
+        </div>
+      </div>
+      
+      <div class="bento-card" data-loader="true">
+        <div class="card-header">
+          <h2 class="card-title">Favorite Songs</h2>
+          <a href="#" class="card-link" data-view="favorite-songs">View All</a>
+        </div>
+        <div id="favoriteSongsSection" class="card-content">
+          <div class="skeleton-loader"></div>
+        </div>
+      </div>
+    </div>
+  `;
         
 
       default:
