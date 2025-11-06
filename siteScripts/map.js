b7f5c0573dbb26d5c149362973b9767a21976bd9
Cole Hanson <4theloveofcarl@gmail.com>
2025-11-06 04:33:29 -0600

    update siteScripts/map.js

--- siteScripts/map.js
+++ siteScripts/map.js
@@ -158,7 +158,7 @@ export const ICONS = Object.freeze({
   more: '<svg fill="currentColor" viewBox="0 0 20 20"><path d="M10 6a2 2 0 110-4 2 2 0 010 4zM10 12a2 2 0 110-4 2 2 0 010 4zM10 18a2 2 0 110-4 2 2 0 010 4z"/></svg>'  
 });
 
-export const MUSICPLAYER = (() => {
+export const MUSIC_PLAYER = (() => {
   const parent = "#drawer";
   return Object.freeze({
     root: parent,
