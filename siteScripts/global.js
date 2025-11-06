18e522528ee7cf502b2d4967e632d0f5deed4c78
Cole Hanson <4theloveofcarl@gmail.com>
2025-11-06 05:08:21 -0600

    update siteScripts/global.js

--- siteScripts/global.js
+++ siteScripts/global.js
@@ -2597,10 +2597,10 @@ const eventHandlers = {
 
   bindControls: () => {
     const nowPlayingTriggers = [DOM.nowPlayingArea, QUERY(NAVBAR.nowPlaying)].filter(Boolean);
-    nowPlayingTriggers.forEach(el => bindClick(el, () => musicPlayer.mainPlayer.toggle()));
+    nowPlayingTriggers.forEach(el => bindClick(el, () => musicPlayer.mainPlayer.togglePlayPause()));
     
     const navbarPlayPause = QUERY(NAVBAR.playPause);
-    if (navbarPlayPause) bindClick(navbarPlayPause, () => musicPlayer.mainPlayer.toggle());
+    if (navbarPlayPause) bindClick(navbarPlayPause, () => musicPlayer.playback.toggle());
     
     const navbarPrevious = QUERY(NAVBAR.previous);
     if (navbarPrevious) bindClick(navbarPrevious, () => musicPlayer.playback.previous());
