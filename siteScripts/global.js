79b263ea683bab1d6718fe2b1174a1fc5c7b9526
Cole Hanson <4theloveofcarl@gmail.com>
2025-11-09 16:41:55 -0600

    update siteScripts/global.js

--- siteScripts/global.js
+++ siteScripts/global.js
@@ -517,13 +517,13 @@ const overlays = {
     unified(title, content) {
       const wrappedContent = `
         <div class="modal-content">
-          <div class='header'>
+          <div class="modal-header">
             <div>
               <h2 class="modal-title">${title}</h2>
             </div>
             <button class="close-btn" data-close aria-label="Close">&times;</button>
           </div>
-          <div class='body'>
+          <div class="modal-body">
             ${content}
           </div>
         </div>
@@ -1506,7 +1506,7 @@ const musicPlayer = {
                     recalc();
                     onScroll();
                 });
-                $byId('musicPlayerCover')?.addEventListener('load', () => {
+                $byId('music-player-cover')?.addEventListener('load', () => {
                     recalc();
                     onScroll();
                 });
