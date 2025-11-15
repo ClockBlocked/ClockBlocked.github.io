3fb14afdee3f1a0dfadf3dda5972d5190f873ada
Cole Hanson <4theloveofcarl@gmail.com>
2025-11-15 12:50:07 -0600

    update siteScripts/global.js

--- siteScripts/global.js
+++ siteScripts/global.js
@@ -6,6 +6,7 @@ import { homePage, views } from './pages/statics.js';
 import { pageLoader, navigation } from './pages/rendering.js';
 import { ui, pageUpdates } from './pages/updates.js';
 import { deepLinkRouter } from './pages/router.js';
+import { notifications } from './pages/router.js';
 
 const ACTION_GRID_ITEMS = [
   { id: 'play-next', icon: 'M9 5l7 7-7 7M15 5v14', label: 'Play Next' },
@@ -678,6 +679,9 @@ const overlays = {
   }
 };
 
+
+
+/**
 const notifications = {
   container: null,
   items: new Set(),
@@ -871,6 +875,9 @@ const notifications = {
       .replaceAll("'", "&#039;");
   }
 };
+**/
+
+
 
 const notificationPlayer = {
   state: {
