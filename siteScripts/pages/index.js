d9d9695b47d397fb1560c932e000ada8d4243c5e
Cole Hanson <4theloveofcarl@gmail.com>
2025-11-18 02:05:59 -0600

    update siteScripts/pages/index.js

--- siteScripts/pages/index.js
+++ siteScripts/pages/index.js
@@ -1,5 +1,5 @@
 // Export all page components
 export { homePage, views } from './statics.js';
 export { escapeForAttribute, pageLoader, navigation } from './rendering.js';
-export { pageUpdates, UI } from './updates.js';
+export { pageUpdates, ui } from './updates.js';
 export { deepLinkRouter } from './router.js';
