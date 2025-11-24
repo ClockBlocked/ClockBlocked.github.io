4b448edbf7ba2fdf1a89405f2f71095b3aca268c
Cole Hanson <4theloveofcarl@gmail.com>
2025-11-24 16:16:38 -0600

    update siteScripts/index.js

--- siteScripts/index.js
+++ siteScripts/index.js
@@ -10,7 +10,7 @@ export {
 export { 
   appState, storage, notificationPlayer, dropdown, 
   overlays, playlists, utils, app, pageLoader, 
-  navigation, ACTION_GRID_ITEMS 
+  navigation, ACTION_GRID_ITEMS, PLAYER_EVENTS
 } from './global.js';
 
 export { notifications } from './toasts.js';
