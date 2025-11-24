9c75c9865424a3c834982fa75033ae62b5fdecae
Cole Hanson <4theloveofcarl@gmail.com>
2025-11-24 17:30:26 -0600

    update siteScripts/index.js

--- siteScripts/index.js
+++ siteScripts/index.js
@@ -1,18 +1,40 @@
-// Export main app components
-export { 
-  DOM, QUERY, QUERY_ALL, IDS, CLASSES, ROUTES, THEMES, STORAGE_KEYS, 
-  ICONS, AUDIO_FORMATS, REPEAT_MODES, NOTIFICATION_TYPES, MUSIC_PLAYER, 
-  NAVBAR, $, $byId, elementCache, clearElementCache, MODALS, TOAST_ICONS, 
-  TOAST_STYLES, $bySelector, $allBySelector, $inContext, getElement, 
-  getElements, getElementInContext, injectIcons 
+// ------------------------------------------------------
+// map.js (core framework-level exports)
+// ------------------------------------------------------
+export {
+  DOM, QUERY, QUERY_ALL, IDS, CLASSES, ROUTES, THEMES, STORAGE_KEYS,
+  ICONS, AUDIO_FORMATS, REPEAT_MODES, NOTIFICATION_TYPES, MUSIC_PLAYER,
+  NAVBAR, $, $byId, elementCache, clearElementCache, MODALS, TOAST_ICONS,
+  TOAST_STYLES, $bySelector, $allBySelector, $inContext, getElement,
+  getElements, getElementInContext, injectIcons
 } from './map.js';
 
-export { 
-  appState, storage, notificationPlayer, dropdown, 
-  overlays, playlists, utils, app, pageLoader, 
-  navigation, ACTION_GRID_ITEMS, PLAYER_EVENTS, PubSub, listRenderer
+// ------------------------------------------------------
+// global.js (main application engine)
+// ------------------------------------------------------
+export {
+  appState,
+  storage,
+  notificationPlayer,
+  dropdown,
+  overlays,
+  playlists,
+  utils,
+  app,
+  pageLoader,
+  navigation,
+  ACTION_GRID_ITEMS,
+  PLAYER_EVENTS,
+  PubSub,
+  listRenderer
 } from './global.js';
 
+// ------------------------------------------------------
+// toasts.js
+// ------------------------------------------------------
 export { notifications } from './toasts.js';
 
+// ------------------------------------------------------
+// musicPlayers.js (player engine)
+// ------------------------------------------------------
 export { musicPlayer } from './musicPlayers.js';
