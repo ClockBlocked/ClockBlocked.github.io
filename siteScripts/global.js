a10f9204ed871f017870572cca3ec0892dfb1794
Cole Hanson <4theloveofcarl@gmail.com>
2025-11-24 21:29:40 -0600

    update siteScripts/global.js

--- siteScripts/global.js
+++ siteScripts/global.js
@@ -29,6 +29,8 @@ import { musicPlayer } from './musicPlayers.js';
 // Critical: import notifications directly
 import { notifications } from './toasts.js';
 
+import { views } from './pages/statics.js';
+
 
 const ACTION_GRID_ITEMS = [
   { id: 'play-next', icon: 'M9 5l7 7-7 7M15 5v14', label: 'Play Next' },
