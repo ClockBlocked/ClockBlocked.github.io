f106345bd45e0b344bc9a0ab2e2d79faf7738ae7
Cole Hanson <4theloveofcarl@gmail.com>
2025-11-25 14:53:09 -0600

    update siteScripts/NEWpages/background.js

--- siteScripts/NEWpages/background.js
+++ siteScripts/NEWpages/background.js
@@ -1,4 +1,6 @@
-import { render, create } from '../utilities/templates.js';
+import { appState, storage, utils, notificationPlayer } from '../global.js';
+import { musicPlayer } from '../musicPlayers.js';
+import { notifications } from '../toasts.js';
 
 export const syncs = {
   breadCrumbs: function(items, options = {}) {
